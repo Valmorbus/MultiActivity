2260958dac2c0fb1eddb9a2c765cf8a676999b4c
Valmorbus <borgstrom.simon@gmail.com>
2016-03-14 19:15:28 +0100

    Initial commit

---
+++ app/src/main/java/com/nackademin/borgstrom/multiactivity/Main2Activity.java
@@ -0,0 +1,36 @@
+package com.nackademin.borgstrom.multiactivity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        String mintext = getIntent().getStringExtra("mintext");
+        TextView tx = (TextView) findViewById(R.id.textView3);
+        tx.setText(mintext);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
